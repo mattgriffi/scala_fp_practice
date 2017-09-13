eaaead2caba14c1072746d85869617a4bd395fb0
mseffner <mseffner@gmail.com>
2017-09-13 08:20:43 -0700

    implemented partial, curry, uncurry, and compose

--- src/MyModule.scala
+++ src/MyModule.scala
@@ -64,4 +64,20 @@ object MyModule {
         if (a.length <= 1) true
         else loop(1)
     }
+
+    def partial1[A, B, C](a: A, f: (A, B) => C): B => C = {
+        (b: B) => f(a, b)
+    }
+
+    def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+        (a: A) => (b: B) => f(a, b)
+    }
+
+    def uncurry[A, B, C](f: A => (B => C)): (A, B) => C = {
+        (a: A, b: B) => f(a)(b)
+    }
+
+    def compose[A, B, C](f: B => C, g: A => B): A => C = {
+        (a: A) => f(g(a))
+    }
 }
