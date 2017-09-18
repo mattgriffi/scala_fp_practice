0cf08a9e15cbe2fe60eec7b3d0f8c0bccd10081d
mseffner <mseffner@gmail.com>
2017-09-18 12:37:57 -0700

    added foldLeft

--- src/List.scala
+++ src/List.scala
@@ -7,6 +7,12 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
 
+    @annotation.tailrec
+    def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B) : B = as match {
+        case Nil => z
+        case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+
     def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
         case Nil => z
         case Cons(x, xs) => f(x, foldRight(xs, z)(f))
