a5d02e8cb797f29b34b861cf9caab73b1c86d01f
mseffner <mseffner@gmail.com>
2017-09-17 20:29:47 -0700

    added drop method to remove the first n elements of a List

--- src/List.scala
+++ src/List.scala
@@ -33,4 +33,11 @@ object List {
         case Cons(_, x) => Cons(head, x)
 
     }
+
+    def drop[A](xs: List[A], n: Int): List[A] = xs match {
+        case Nil => Nil
+        case Cons(_, x) =>
+            if (n <= 0)  xs
+            else  drop(x, n - 1)
+    }
 }
